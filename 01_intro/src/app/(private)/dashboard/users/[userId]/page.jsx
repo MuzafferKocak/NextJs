daa2ef5500af7c64bb0fc5bc24dd59014c2e8b01
Mahir <mkojak75@gmail.com>
2024-10-12 22:25:49 +0200

    staticGenerate

--- 01_intro/src/app/(private)/dashboard/users/[userId]/page.jsx
+++ 01_intro/src/app/(private)/dashboard/users/[userId]/page.jsx
@@ -1,15 +1,20 @@
-import GoBack from '@/components/GoBack'
-import React from 'react'
+import GoBack from "@/components/GoBack";
+import React from "react";
 
-const UserDetail = ({params}) => {
-    const {userId} = params
+const UserDetail = ({ params }) => {
+  const { userId } = params;
   return (
     <div>
-        <h1 className='text-2xl text-center'>User Detail</h1>
-        <p>user- {userId}</p>
-        <GoBack/>
+      <h1 className="text-2xl text-center">User Detail</h1>
+      <p>user- {userId}</p>
+      <GoBack />
     </div>
-  )
-}
+  );
+};
 
-export default UserDetail
+export default UserDetail;
+
+export async function generateStaticParams() {
+  const userArr = [1, 2, 3, 4];
+  return userArr.map((userId) => ({ userId: userId.toString() }));
+}
